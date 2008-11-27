86de6728e2782ff9b6793833e616697f79cf5530
hhhulkko <devnull@localhost>
2008-11-27 12:18:16 +0000

    Improved acceptance tests for "Select From Popup Menu On Selected Tree Nodes" keyword.

--- test-keywords/src/main/java/org/robotframework/swing/keyword/testing/TreeTestingKeywords.java
+++ test-keywords/src/main/java/org/robotframework/swing/keyword/testing/TreeTestingKeywords.java
@@ -1,37 +1,19 @@
 package org.robotframework.swing.keyword.testing;
 
-import java.util.List;
-
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-
 import org.robotframework.javalib.annotation.RobotKeyword;
 import org.robotframework.javalib.annotation.RobotKeywords;
+import org.robotframework.javalib.util.ArrayUtil;
 import org.robotframework.swing.testapp.TestTreeResults;
-import org.robotframework.swing.tree.TreeOperator;
-import org.robotframework.swing.tree.TreeOperatorFactory;
 
 @RobotKeywords
 public class TreeTestingKeywords {
-    @RobotKeyword("Adds the specified amount of children to the selected node, fails if no nodes are selected.\n.")
-    public void addChildren(String childCount) {
-        TreeOperator treeOperator = new TreeOperatorFactory().createOperatorByIndex(0);
-        DefaultMutableTreeNode lastPathComponent = (DefaultMutableTreeNode) treeOperator.getSelectionPath()
-            .getLastPathComponent();
-        
-        for (int i = 0; i < Integer.parseInt(childCount); i++) {
-            lastPathComponent.add(new DefaultMutableTreeNode("child" + i));
-        }
-        ((DefaultTreeModel) treeOperator.getModel()).nodeStructureChanged(lastPathComponent);
-    }
-    
-    @RobotKeyword("For testing 'SelectFromPopupMenuOnSelectedTreeNodes'.\n")
-    public List<String> getSavedNodes() {
-        return TestTreeResults.nodes;
-    }
-    
     @RobotKeyword
     public void clearSavedNodes() {
         TestTreeResults.nodes.clear();
+    }
+    
+    @RobotKeyword
+    public void savedNodesShouldBe(String[] expectedNodes) {
+        ArrayUtil.assertArraysContainSame(expectedNodes, TestTreeResults.nodes.toArray(new String[0]));
     }
 }
