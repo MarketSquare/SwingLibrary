1c6b1b1711e2c5a53b5d256008b35fd7e43ea0ab
hhhulkko <devnull@localhost>
2009-01-07 12:40:53 +0000

    Added testingkeywords for textfields.

---
+++ test-keywords/src/main/java/org/robotframework/swing/keyword/testing/TextfieldTestingKeywords.java
@@ -0,0 +1,23 @@
+package org.robotframework.swing.keyword.testing;
+
+import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywords;
+import org.robotframework.swing.textcomponent.TextComponentOperator;
+import org.robotframework.swing.textcomponent.TextComponentOperatorFactory;
+
+@RobotKeywords
+public class TextfieldTestingKeywords {
+    @RobotKeyword
+    public void disableTextField(String identifier) {
+        createOperator(identifier).setEnabled(false);
+    }
+
+    @RobotKeyword
+    public void enableTextField(String identifier) {
+        createOperator(identifier).setEnabled(true);
+    }
+    
+    private TextComponentOperator createOperator(String identifier) {
+        return new TextComponentOperatorFactory().createOperator(identifier);
+    }
+}
