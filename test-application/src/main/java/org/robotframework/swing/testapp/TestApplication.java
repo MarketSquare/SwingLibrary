b759d821b034f1c3069f3eb683150f2afa71fd90
KariHusa <devnull@localhost>
2009-12-21 08:25:55 +0000

    Unused imports

--- test-application/src/main/java/org/robotframework/swing/testapp/TestApplication.java
+++ test-application/src/main/java/org/robotframework/swing/testapp/TestApplication.java
@@ -11,9 +11,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListCellRenderer;
