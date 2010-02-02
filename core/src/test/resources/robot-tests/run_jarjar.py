3b4be699b547fed141a2a8c93a29e3a064ade9ca
jprantan <devnull@localhost>
2010-02-02 13:02:54 +0000

    Updated to wotk

--- core/src/test/resources/robot-tests/run_jarjar.py
+++ core/src/test/resources/robot-tests/run_jarjar.py
@@ -3,18 +3,17 @@
 import os
 import sys
 from glob import glob
+from tempfile import gettempdir
 
 def main(args):
     root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 
                                         '..', '..', '..'))
     jarjar = glob(os.path.join(root, 'target', '*jarjar.jar'))[0]
     test_app = os.path.join(root, 'test-application', 'target', 'classes')
-    test_kws = glob(os.path.join(root, 'test-keywords', 'target', 'swinglibrary-test-keywords-*-jarjar.jar'))[0]
-    jars = [jarjar, test_app]
+    test_kws = os.path.join(root, 'test-keywords', 'target', 'jarjared-swinglibrary-test-keywords.jar')
     jars = [jarjar, test_app, test_kws]
     os.environ['CLASSPATH'] = os.pathsep.join(jars)
-    outputdir = os.path.join(root, 'results')
-    os.system('jybot --loglevel TRACE --critical regression --outputdir "%s" %s' % (outputdir, ' '.join(args)))
+    os.system('jybot --loglevel TRACE --critical regression --outputdir "%s" %s' % (gettempdir(), ' '.join(args)))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- jarjar.sh
+++ jarjar.sh
@@ -14,6 +14,6 @@ echo unzip -d $target $jar META-INF/MANIFEST.MF
 unzip -d $target $jar META-INF/MANIFEST.MF
 echo jar ufm $jarjar $target/META-INF/MANIFEST.MF
 jar ufm $jarjar $target/META-INF/MANIFEST.MF
-echo jarjarring the test-keywords... 
-java -jar release/jarjar-1.0.jar process release/jarjar_rules.txt test-keywords/target/swinglibrary-test-keywords-1.1-RC2.jar test-keywords/target/swinglibrary-test-keywords-1.1-RC2-jarjar.jar
+echo jarjarring the test-keywords...
+java -jar release/jarjar-1.0.jar process release/jarjar_rules.txt test-keywords/target/swinglibrary-test-keywords*.jar test-keywords/target/jarjared-swinglibrary-test-keywords.jar
 echo test-keywords jarjarred.
