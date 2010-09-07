b365ed51a78b4d4d5203c489fc0dc99e6166844c
KariHusa <devnull@localhost>
2010-09-07 10:14:38 +0000

    Paranaming through maven

--- create_doc.py
+++ create_doc.py
@@ -1,8 +1,6 @@
 import os
 import re
 import sys
-import glob
-import subprocess
 
 base = os.path.abspath(os.path.dirname(__file__))
 
@@ -25,8 +23,7 @@ def sh(command):
     return output
 
 def add_dependencies_to_classpath():
-    paranamer_jar = os.path.join(base, 'lib', 'swing-library-paranamer.jar')
-    dependencies =  get_deps() + [paranamer_jar] + [os.path.join(base, 'core', 'target', 'classes')]
+    dependencies =  get_deps() + [os.path.join(base, 'core', 'target', 'classes')]
     for deb in dependencies:
         sys.path.append(deb)
     os.environ['CLASSPATH'] = os.pathsep.join(dependencies)
@@ -38,22 +35,8 @@ def assert_doc_ok():
         if '*<unknown>' in line:
             raise "Errors in documentation: " + doc_name + " contains *<unknown>-tags."
 
-def generate_parameter_names():
-    paraname_file = os.path.join(base, 'core', 'target', '.paraname')
-    if not os.path.exists(paraname_file):
-        open(paraname_file, 'a')
-        run_paranamer()
-
-def run_paranamer():
-    this = os.path.join(base, 'core', 'src', 'main', 'java')
-    to = os.path.join(base, 'core', 'target', 'classes')
-    from com.nsn.robot.paraname import Paranamer
-    gen = Paranamer()
-    gen.processSourcePath(this, to)
-
 def doc():
     add_dependencies_to_classpath()
-    generate_parameter_names()
     libdoc = os.path.join(base, 'lib', 'libdoc', 'libdoc.py')
     output = os.path.join(base, 'doc', 'SwingLibrary-%s-doc.html' % (VERSION))
     command = 'jython -Dpython.path=%s %s --output %s %s' % (os.path.join(base, 'lib', 'robotframework-2.5.2.jar', 'Lib'), libdoc, output, 'SwingLibrary')
