a79571cae102a241ee8b036403ff9aa5ff53ef77
jprantan <devnull@localhost>
2010-09-07 09:37:01 +0000

    Finally doc works as part of dist.

--- create_doc.py
+++ create_doc.py
@@ -8,35 +8,15 @@ base = os.path.abspath(os.path.dirname(__file__))
 
 VERSION = '1.1.2-SNAPSHOT'
 
-def get_swinglib_jar():
-    pattern = os.path.join(os.path.dirname(__file__),
-                           os.path.join('core', 'target'), '*-jar-with-dependencies.jar')
-    paths = glob.glob(pattern)
-    if paths:
-        paths.sort()
-        return paths[-1]
-    else:
-        raise RuntimeError('Please run "mvn -f core/pom.xml assembly:assembly" first')
-
 def exists(file_name):
     file = os.path.join(base, file_name)
     return os.path.exists(file)
 
-def get_test_deps():
-    deps = open('dependencies.txt', 'rb').read().splitlines()
-    return [ dep for dep in deps 
-             if 'beaninject' in dep 
-             or 'hamcrest' in dep 
-             or 'jdave' in dep 
-             or 'jmock' in dep 
-             or 'junit' in dep 
-             or 'cglib' in dep 
-             or 'objenesis' in dep 
-             or 'javatools-test' in dep
-             or 'paranamer' in dep
-             or 'paranamer-generator' in dep
-             or 'qdox' in dep
-             or 'asm' in dep ]
+def get_deps():
+    os.environ['MAVEN_OPTS'] = '-DoutputAbsoluteArtifactFilename=true'
+    mvn_output = sh('mvn -f core/pom.xml dependency:list').splitlines()
+    jars = [re.sub('.*:((:?C:)?)', '\\1', file) for file in mvn_output if re.search('jar', file)]
+    return jars
 
 def sh(command):
     process = os.popen(command)
@@ -45,19 +25,8 @@ def sh(command):
     return output
 
 def add_dependencies_to_classpath():
-    if not exists('dependencies.txt'):
-        os.environ['MAVEN_OPTS'] = '-DoutputAbsoluteArtifactFilename=true'
-        mvn_output = sh('mvn -f core/pom.xml dependency:list').splitlines()
-
-        jars = [re.sub('.*:((:?C:)?)', '\\1', file) for file in mvn_output if re.search('jar', file)]
-        dependencies_txt = open(os.path.join(base, 'dependencies.txt'), 'w')
-        for jar in jars:
-            if exists(jar):
-                dependencies_txt.write(jar + '\n')
-        dependencies_txt.flush()
-
     paranamer_jar = os.path.join(base, 'lib', 'swing-library-paranamer.jar')
-    dependencies =  [get_swinglib_jar()] + get_test_deps() + [paranamer_jar]
+    dependencies =  get_deps() + [paranamer_jar] + [os.path.join(base, 'core', 'target', 'classes')]
     for deb in dependencies:
         sys.path.append(deb)
     os.environ['CLASSPATH'] = os.pathsep.join(dependencies)
@@ -69,31 +38,29 @@ def assert_doc_ok():
         if '*<unknown>' in line:
             raise "Errors in documentation: " + doc_name + " contains *<unknown>-tags."
 
-def generate_parameter_names(this, to):
+def generate_parameter_names():
     paraname_file = os.path.join(base, 'core', 'target', '.paraname')
     if not os.path.exists(paraname_file):
         open(paraname_file, 'a')
-        run_paranamer(this, to)
+        run_paranamer()
 
-def run_paranamer(this, to):
+def run_paranamer():
+    this = os.path.join(base, 'core', 'src', 'main', 'java')
+    to = os.path.join(base, 'core', 'target', 'classes')
     from com.nsn.robot.paraname import Paranamer
     gen = Paranamer()
     gen.processSourcePath(this, to)
 
 def doc():
     add_dependencies_to_classpath()
-    generate_parameter_names(os.path.join(base, 'core', 'src', 'main', 'java'), os.path.join(base, 'core', 'target', 'classes'))
+    generate_parameter_names()
     libdoc = os.path.join(base, 'lib', 'libdoc', 'libdoc.py')
     output = os.path.join(base, 'doc', 'SwingLibrary-%s-doc.html' % (VERSION))
     command = 'jython -Dpython.path=%s %s --output %s %s' % (os.path.join(base, 'lib', 'robotframework-2.5.2.jar', 'Lib'), libdoc, output, 'SwingLibrary')
     print command
     return os.system(command)
 
-def assemble_core():
-    subprocess.call(['mvn', '-Ddist.version=%s' % VERSION, '-f', 'core/pom.xml', 'assembly:assembly'])
-
 if __name__ == '__main__':
-    assemble_core()
     ret_code = doc()
     assert_doc_ok()
     sys.exit(ret_code)
