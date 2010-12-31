6ceb7c586c1cb87680ec9e51083c3a8884327baa
jprantan <devnull@localhost>
2010-12-31 08:50:23 +0000

    Added check that selecting disabled menu item fails. Done issue 180.

--- core/src/main/java/org/robotframework/swing/keyword/menu/MenuKeywords.java
+++ core/src/main/java/org/robotframework/swing/keyword/menu/MenuKeywords.java
@@ -33,6 +33,7 @@ public class MenuKeywords extends MenuSupport {
         + "| Select Window    | _My Application_           |\n"
         + "| Select From Menu | _Tools|Testing|MyTestTool_ |\n")
     public void selectFromMenu(String menuPath) {
+        menuItemShouldBeEnabled(menuPath);
         showMenuItem(menuPath).pushNoBlock();
     }
 
@@ -43,6 +44,7 @@ public class MenuKeywords extends MenuSupport {
         + "| Select Window             | _My Application_           |\n"
         + "| Select From Menu And Wait | _Tools|Testing|MyTestTool_ |\n")
     public void selectFromMenuAndWait(String menuPath) {
+        menuItemShouldBeEnabled(menuPath);
         showMenuItem(menuPath).push();
     }
 
