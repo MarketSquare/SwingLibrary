6709247f4039246aa61a9702a45cfdfb815aa0c7
jprantan <devnull@localhost>
2011-01-03 13:38:37 +0000

    cleanup

--- test-application/src/main/java/org/robotframework/swing/testapp/TestApplication.java
+++ test-application/src/main/java/org/robotframework/swing/testapp/TestApplication.java
@@ -35,7 +35,7 @@ public class TestApplication {
     private JPanel panel;
     private JFrame frame;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         new TestApplication().runTestApplication();
     }
 
