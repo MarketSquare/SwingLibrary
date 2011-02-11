682cdfd4ebbeee931bef3e161f8d5057ea6af62b
janne.t.harkonen <devnull@localhost>
2011-02-11 07:55:35 +0000

    removed test-keywords and test-application subprojects

--- test-application/src/main/java/org/robotframework/swing/extension/ExtendedSwingLibrary2.java
+++
@@ -1,9 +0,0 @@
-package org.robotframework.swing.extension;
-
-import org.robotframework.swing.SwingLibrary;
-
-public class ExtendedSwingLibrary2 extends SwingLibrary {
-    public ExtendedSwingLibrary2() {
-        super("com/some/own/keyword/*.class");
-    }
-}
--- test-application/src/main/java/org/robotframework/swing/testapp/Operation.java
+++
@@ -1,7 +0,0 @@
-package org.robotframework.swing.testapp;
-
-import java.awt.Component;
-
-public interface Operation {
-    void perform(Component operatedComponent);
-}
