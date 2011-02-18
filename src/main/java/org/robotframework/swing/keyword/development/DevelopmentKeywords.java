b8477084b56a33d88c82f0e9809580d8cbcf5244
KariHusa <devnull@localhost>
2011-02-18 11:51:29 +0000

    listComponentsInContext: Add component indexing by categories, initial draft

--- src/main/java/org/robotframework/swing/keyword/development/DevelopmentKeywords.java
+++ src/main/java/org/robotframework/swing/keyword/development/DevelopmentKeywords.java
@@ -19,7 +19,34 @@ package org.robotframework.swing.keyword.development;
 import java.awt.Component;
 import java.awt.Container;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.swing.CellRendererPane;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JList;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JRootPane;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.JTree;
+import javax.swing.JViewport;
 
 import org.robotframework.javalib.annotation.RobotKeyword;
 import org.robotframework.javalib.annotation.RobotKeywords;
@@ -43,19 +70,16 @@ public class DevelopmentKeywords {
     }
 
     private void printSpacesToFormatOutputAsTree(int level) {
-        for (int i = 0; i < level; i++) {
+        for (int i = 0; i < level; i++)
             System.out.print("   ");
-        }
     }
 
     private String componentToString(Component component) {
         String componentString = component.toString();
         int indexToStartOfDetails = componentString.indexOf('[');
-        if (indexToStartOfDetails == -1) {
+        if (indexToStartOfDetails == -1)
             return componentString;
-        } else {
-            return componentString.substring(0, indexToStartOfDetails);
-        }
+        return componentString.substring(0, indexToStartOfDetails);
     }
     
     private class ContainerIteratorForListing extends ContainerIterator {
@@ -66,7 +90,7 @@ public class DevelopmentKeywords {
         public void operateOnComponent(Component component, int level) {
             printSpacesToFormatOutputAsTree(level);
             String componentName = componentToString(component);
-            System.out.println(level + " " + componentName + ": " + component.getName());
+            System.out.println(level + " " + componentName + " " + indexOfTypeOf(component) + ": " + component.getName());
             resultComponentList.add(componentName);
         }
     }
@@ -96,5 +120,44 @@ public class DevelopmentKeywords {
                 }
             }
         }
+    }
+
+    private String indexOfTypeOf(Component component) {
+        if (component instanceof JButton) return indexOfComponentOfClass(JButton.class);
+        if (component instanceof JCheckBox) return indexOfComponentOfClass(JCheckBox.class);
+        if (component instanceof JRadioButton) return indexOfComponentOfClass(JRadioButton.class);
+        if (component instanceof JToggleButton) return indexOfComponentOfClass(JToggleButton.class);
+        if (component instanceof JFileChooser) return indexOfComponentOfClass(JFileChooser.class);
+        if (component instanceof JTable) return indexOfComponentOfClass(JTable.class);
+        if (component instanceof JList) return indexOfComponentOfClass(JList.class);
+        if (component instanceof JTextField) return indexOfComponentOfClass(JTextField.class);
+        if (component instanceof JTextArea) return indexOfComponentOfClass(JTextArea.class);
+        if (component instanceof JTree) return indexOfComponentOfClass(JTree.class);
+        if (component instanceof JInternalFrame) return indexOfComponentOfClass(JInternalFrame.class);
+        if (component instanceof JFrame) return indexOfComponentOfClass(JFrame.class);
+        if (component instanceof JPanel) return indexOfComponentOfClass(JPanel.class);
+        if (component instanceof JComboBox) return indexOfComponentOfClass(JComboBox.class);
+        if (component instanceof JSpinner) return indexOfComponentOfClass(JSpinner.class);
+        if (component instanceof JLabel) return indexOfComponentOfClass(JLabel.class);
+        if (component instanceof JTabbedPane) return indexOfComponentOfClass(JTabbedPane.class);
+        if (component instanceof JRootPane) return indexOfComponentOfClass(JRootPane.class);
+        if (component instanceof JLayeredPane) return indexOfComponentOfClass(JLayeredPane.class);
+        if (component instanceof CellRendererPane) return indexOfComponentOfClass(CellRendererPane.class);
+        if (component instanceof JScrollPane) return indexOfComponentOfClass(JScrollPane.class);
+        if (component instanceof JMenuBar) return indexOfComponentOfClass(JMenuBar.class);
+        if (component instanceof JViewport) return indexOfComponentOfClass(JViewport.class);
+        if (component instanceof JScrollBar) return indexOfComponentOfClass(JScrollBar.class);
+        return "N/A";
+    }
+    
+    private Map<Class<? extends Component>, Integer> indexesByType = new HashMap<Class<? extends Component>, Integer>();
+    private String indexOfComponentOfClass(Class<? extends Component> clazz) {
+        int index;
+        if (indexesByType.containsKey(clazz))
+            index = indexesByType.get(clazz) + 1;
+        else
+            index = 0;
+        indexesByType.put(clazz, new Integer(index));
+        return ""+index;
     }
 }
