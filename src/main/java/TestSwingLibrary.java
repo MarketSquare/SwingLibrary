b309b222bc226ac87fb163e5ca6233b96595b854
KariHusa <devnull@localhost>
2011-03-03 13:34:56 +0000

    TestSwingLibrary: Add library for collecting the keywords used in swinglibrary tests

---
+++ src/main/java/TestSwingLibrary.java
@@ -0,0 +1,8 @@
+
+public class TestSwingLibrary extends org.robotframework.swing.SwingLibrary {
+
+    public TestSwingLibrary() {
+        super("org/robotframework/swing/testkeyword/**/*.class");
+    }
+    
+}
