4fae980e7c7e4ddfd2c27fdb6d5d55f3c7d0fe24
janne.t.harkonen <devnull@localhost>
2011-03-30 11:06:29 +0000

    specs: Removed tests
    The functionality tested by these tests has been moved,
    so these tests are no longer necesary.

--- src/test/java/org/robotframework/swing/keyword/table/TableKeywordsSpec.java
+++ src/test/java/org/robotframework/swing/keyword/table/TableKeywordsSpec.java
@@ -45,7 +45,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
         public void hasSelectTableCellAreaKeyword() {
             specify(context, satisfies(new RobotKeywordContract("selectTableCellArea")));
         }
-        
+
         public void hasClearTableSelectionKeyword() {
             specify(context, satisfies(new RobotKeywordContract("clearTableSelection")));
         }
@@ -81,35 +81,35 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
         public void hasClearTableCellValueKeyword() {
             specify(context, satisfies(new RobotKeywordContract("clearTableCell")));
         }
-        
+
         public void hasSelectFromTableCellPopupMenuKeyword() {
             specify(context, satisfies(new RobotKeywordContract("selectFromTableCellPopupMenu")));
         }
-        
+
         public void hasTableCellPopupMenuShouldBeEnabledKeyword() {
             specify(context, satisfies(new RobotKeywordContract("tableCellPopupMenuShouldBeEnabled")));
         }
-        
+
         public void hasTableCellPopupMenuShouldBeDisabledKeyword() {
             specify(context, satisfies(new RobotKeywordContract("tableCellPopupMenuShouldBeDisabled")));
         }
-        
+
         public void hasTypeIntoTableCellKeyword() {
             specify(context, satisfies(new RobotKeywordContract("typeIntoTableCell")));
         }
-        
+
         public void hasFindTableRowKeyword() {
             specify(context, satisfies(new RobotKeywordContract("findTableRow")));
         }
-        
+
         public void hasGetTableHeadersKeyword() {
             specify(context, satisfies(new RobotKeywordContract("getTableHeaders")));
         }
-        
+
         public void hasGetTableColumnValuesKeyword() {
             specify(context, satisfies(new RobotKeywordContract("getTableColumnValues")));
         }
-        
+
         public void hasGetTableCellPropertiesKeyword() {
             specify(context, satisfies(new RobotKeywordContract("getTableCellProperty")));
         }
@@ -121,12 +121,12 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
                 return "someValue";
             }
         };
-        
+
         public TableKeywords create() {
             injectMockOperatorFactory();
             return tableKeywords;
         }
-        
+
         public void getsSelectedTableCellValue() {
             checking(new Expectations() {{
                 one(tableOperator).getSelectedCellValue();
@@ -135,47 +135,38 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
 
             specify(context.getSelectedTableCellValue(tableIdentifier), must.equal(cellValue.toString()));
         }
-        
-        public void getsTableCellValue() {
-            checking(new Expectations() {{
-                one(tableOperator).getCellValue(row, columnIdentifier);
-                will(returnValue(cellValue));
-            }});
 
-            specify(context.getTableCellValue(tableIdentifier, row, columnIdentifier), must.equal(cellValue.toString()));
-        }
-        
 		public void getsTableCellProperties() {
 			@SuppressWarnings("serial")
             final Map<String, Object> props = new HashMap<String, Object>() {{
                 put("background", Color.black);
             }};
-            
+
             checking(new Expectations() {{
                 one(tableOperator).getCellProperties(row, columnIdentifier);
-                will(returnValue(props)); 
+                will(returnValue(props));
             }});
-            
+
             Object cellProperty = context.getTableCellProperty(tableIdentifier, row, columnIdentifier, "background");
             specify(cellProperty, Color.black);
         }
     }
-    
+
     public class OperatingOnTable {
         public TableKeywords create() {
             injectMockOperatorFactory();
             return tableKeywords;
         }
-        
+
         public void getsTableHeaders() {
             final String[] tableHeaders = new String[] {"1", "2", "3"};
             checking(new Expectations() {{
                 one(tableOperator).getTableHeaders(); will(returnValue(tableHeaders));
             }});
-            
+
             specify(context.getTableHeaders(tableIdentifier), containsInOrder("1", "2", "3"));
         }
-        
+
         public void getsTableColumnCount() {
             checking(new Expectations() {{
                 one(tableOperator).getColumnCount(); will(returnValue(7));
@@ -191,7 +182,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
 
             specify(context.getTableRowCount(tableIdentifier), must.equal(7));
         }
-        
+
         public void findsRow() {
             checking(new Expectations() {{
                 one(tableOperator).findCellRow("someValue"); will(returnValue(7));
@@ -225,7 +216,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
                 }
             }, must.raise(RuntimeException.class));
         }
-        
+
         public void typesIntoTableCell() {
             final String newValue = "newValue";
 
@@ -242,7 +233,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
             }});
         	context.clickOnTableCell(tableIdentifier, row, "0", new String[0]);
         }
-        
+
         public void clicksOnTableCellWithKeyAliases() {
         	checking(new Expectations() {{
                 one(tableOperator).clickOnCell(row, "column_identifier", "7", "CTRL_MASK",
@@ -258,7 +249,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
             }});
         	context.clickOnTableCell(tableIdentifier, row, "0", new String[] {"2"});
         }
-        
+
         public void doubleClicksOnTableCellWithSeveralButtons() {
         	checking(new Expectations() {{
                 one(tableOperator).clickOnCell(row, "column_identifier", "2", "BUTTON2_MASK",
@@ -267,7 +258,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
         	context.clickOnTableCell(tableIdentifier, row, "column_identifier",
         			                 new String[] {"2", "RIGHT BUTTON", "CTRL_MASK", "SHIFT", "META_MASK"});
         }
-        
+
         public void selectsTableCell() {
             checking(new Expectations() {{
                 one(tableOperator).selectCell(row, columnIdentifier);
@@ -275,7 +266,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
 
             context.selectTableCell(tableIdentifier, row, columnIdentifier);
         }
-        
+
         public void selectsTableCellArea() {
             final String columnStart = "0";
             final String columnEnd = "1";
@@ -284,16 +275,16 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
             checking(new Expectations() {{
                 one(tableOperator).selectCellArea(columnStart, columnEnd, rowStart, rowEnd);
             }});
-            
+
             context.selectTableCellArea(tableIdentifier, columnStart, columnEnd, rowStart, rowEnd);
         }
-        
+
         public void getsTableColumnValues() {
             checking(new Expectations() {{
                 one(tableOperator).getColumnValues(columnIdentifier);
                 will(returnValue(new Object[] { "one", "two", "three" }));
             }});
-            
+
             specify(context.getTableColumnValues(tableIdentifier, columnIdentifier), containsExactly("one", "two", "three"));
         }
 
@@ -317,7 +308,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
                 }
             }, must.not().raise(AssertionFailedError.class));
         }
-        
+
         public void tableCellShouldBeEditableFailsWhenItIsNot() throws Throwable {
             checking(new Expectations() {{
                 one(tableOperator).isCellEditable(row, columnIdentifier);
@@ -330,7 +321,7 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
                 }
             }, must.raise(AssertionFailedError.class));
         }
-        
+
         public void tableCellShouldBeSelectedPassesIfCellIsSelected() throws Throwable {
             checking(new Expectations() {{
                 one(tableOperator).isCellSelected(row, columnIdentifier);
@@ -387,87 +378,87 @@ public class TableKeywordsSpec extends MockSupportSpecification<TableKeywords> {
             checking(new Expectations() {{
                 one(tableOperator).clearCell(row, columnIdentifier);
             }});
-            
+
             context.clearTableCell(tableIdentifier, row, columnIdentifier);
         }
     }
-    
+
     public class CallingPopupOnTableCell {
         private String menuPath = "some|menu";
         private JPopupMenuOperator popupMenuOperator;
         private JMenuItemOperator menuItemOperator;
-        
+
         public TableKeywords create() {
             injectMockOperatorFactory();
-            
+
             popupMenuOperator = mock(JPopupMenuOperator.class);
             menuItemOperator = mock(JMenuItemOperator.class);
-            
+
             checking(new Expectations() {{
                 one(tableOperator).callPopupOnCell(row, columnIdentifier);
                 will(returnValue(popupMenuOperator));
-                
+
                 one(popupMenuOperator).showMenuItem(with(equal(menuPath)), with(any(EqualsStringComparator.class)));
-                
+
                 will(returnValue(menuItemOperator));
             }});
-            
+
             return tableKeywords;
         }
-        
+
         public void selectsFromTableCellPopupMenu() {
-            checking(new Expectations() {{    
+            checking(new Expectations() {{
                 one(menuItemOperator).push();
             }});
-            
+
             context.selectFromTableCellPopupMenu(tableIdentifier, row, columnIdentifier, menuPath);
         }
-        
+
         public void tableCellPopupMenuShouldBeEnabledPassesWhenMenuItemIsEnabled() throws Throwable {
-            checking(new Expectations() {{    
+            checking(new Expectations() {{
                 one(menuItemOperator).isEnabled(); will(returnValue(true));
             }});
-            
+
             specify(new Block() {
                 public void run() throws Throwable {
                     context.tableCellPopupMenuShouldBeEnabled(tableIdentifier, row, columnIdentifier, menuPath);
                 }
             }, must.not().raiseAnyException());
         }
-        
+
         public void tableCellPopupMenuShouldBeEnabledFailsWhenMenuItemIsDisabled() throws Throwable {
-            checking(new Expectations() {{    
+            checking(new Expectations() {{
                 one(menuItemOperator).isEnabled(); will(returnValue(false));
             }});
-            
+
             String expectedMessage = "Menuitem '" + menuPath + "' at '" + row + ", " +  columnIdentifier + "' is disabled.";
-            
+
             specify(new Block() {
                 public void run() throws Throwable {
                     context.tableCellPopupMenuShouldBeEnabled(tableIdentifier, row, columnIdentifier, menuPath);
                 }
             }, must.raiseExactly(AssertionFailedError.class, expectedMessage));
         }
-        
+
         public void tableCellPopupMenuShouldBeDisabledPassesWhenMenuItemIsDisabled() throws Throwable {
-            checking(new Expectations() {{    
+            checking(new Expectations() {{
                 one(menuItemOperator).isEnabled(); will(returnValue(false));
             }});
-            
+
             specify(new Block() {
                 public void run() throws Throwable {
                     context.tableCellPopupMenuShouldBeDisabled(tableIdentifier, row, columnIdentifier, menuPath);
                 }
             }, must.not().raiseAnyException());
         }
-        
+
         public void tableCellPopupMenuShouldBeDisabledFailsWhenMenuItemIsEnabled() throws Throwable {
-            checking(new Expectations() {{    
+            checking(new Expectations() {{
                 one(menuItemOperator).isEnabled(); will(returnValue(true));
             }});
-            
+
             String expectedMessage = "Menuitem '" + menuPath + "' at '" + row + ", " +  columnIdentifier + "' is enabled.";
-            
+
             specify(new Block() {
                 public void run() throws Throwable {
                     context.tableCellPopupMenuShouldBeDisabled(tableIdentifier, row, columnIdentifier, menuPath);
--- src/test/java/org/robotframework/swing/table/TableOperatorSpec.java
+++ src/test/java/org/robotframework/swing/table/TableOperatorSpec.java
@@ -3,8 +3,6 @@ package org.robotframework.swing.table;
 import java.awt.Component;
 import java.awt.Point;
 import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Vector;
 
 import javax.swing.JPopupMenu;
@@ -14,7 +12,6 @@ import javax.swing.table.TableColumnModel;
 
 import jdave.Block;
 import jdave.junit4.JDaveRunner;
-
 import org.hamcrest.Matcher;
 import org.hamcrest.core.IsInstanceOf;
 import org.jmock.Expectations;
@@ -23,7 +20,6 @@ import org.netbeans.jemmy.operators.JTableOperator;
 import org.netbeans.jemmy.operators.JTableOperator.TableCellChooser;
 import org.robotframework.jdave.mock.MockSupportSpecification;
 import org.robotframework.swing.comparator.EqualsStringComparator;
-import org.robotframework.swing.util.PropertyExtractor;
 
 @RunWith(JDaveRunner.class)
 public class TableOperatorSpec extends MockSupportSpecification<TableOperator> {
@@ -31,19 +27,19 @@ public class TableOperatorSpec extends MockSupportSpecification<TableOperator> {
     private JTableOperator jTableOperator;
     private Object cellValue = new Object();
     private Point coordinates = new Point(2, 3);
-    
+
     public class Any {
         public void requiresExactMatchWhenComparesValues() {
             jTableOperator = mock(JTableOperator.class);
-            
+
             checking(new Expectations() {{
                 one(jTableOperator).setComparator(with(any(EqualsStringComparator.class)));
             }});
-            
+
             new TableOperator(jTableOperator);
         }
     }
-    
+
     public class OperatingOnRowsAndColumn {
         public TableOperator create() {
             jTableOperator = mock(JTableOperator.class);
@@ -52,202 +48,192 @@ public class TableOperatorSpec extends MockSupportSpecification<TableOperator> {
             }});
             return new TableOperator(jTableOperator);
         }
-        
+
         public void clearsSelection() {
             checking(new Expectations() {{
-                one(jTableOperator).clearSelection(); 
+                one(jTableOperator).clearSelection();
             }});
-            
+
             context.clearSelection();
         }
-        
+
         public void findsCellRow() {
             checking(new Expectations() {{
-                one(jTableOperator).findCellRow("someValue"); will(returnValue(3)); 
+                one(jTableOperator).findCellRow("someValue");
+                will(returnValue(3));
             }});
-            
+
             specify(context.findCellRow("someValue"), 3);
         }
-        
+
         public void getsRowCount() {
             checking(new Expectations() {{
-                one(jTableOperator).getRowCount(); will(returnValue(3)); 
+                one(jTableOperator).getRowCount();
+                will(returnValue(3));
             }});
-            
+
             specify(context.getRowCount(), 3);
         }
-        
+
         public void getsColumnCount() {
             checking(new Expectations() {{
-                one(jTableOperator).getColumnCount(); will(returnValue(3)); 
+                one(jTableOperator).getColumnCount();
+                will(returnValue(3));
             }});
-            
+
             specify(context.getColumnCount(), 3);
         }
-        
+
         public void getsSource() {
             final Component source = dummy(Component.class);
             checking(new Expectations() {{
-                one(jTableOperator).getSource(); will(returnValue(source)); 
+                one(jTableOperator).getSource();
+                will(returnValue(source));
             }});
-            
+
             specify(context.getSource(), source);
         }
-        
+
         public void selectingNonexistentCellFails() {
-        	checking(new Expectations() {{
-        		one(jTableOperator).findCell(with(any(TableCellChooser.class))); will(returnValue(new Point(-1, -1)));
-        	}});
-        	
+            checking(new Expectations() {{
+                one(jTableOperator).findCell(with(any(TableCellChooser.class)));
+                will(returnValue(new Point(-1, -1)));
+            }});
+
             specify(new Block() {
                 public void run() throws Throwable {
                     context.selectCell("12", "nonexisting");
                 }
             }, should.raise(InvalidCellException.class));
         }
-        
-        
+
+
         public void getsTableHeaders() {
             final JTableHeader tableHeader = mock(JTableHeader.class);
             final TableColumnModel columnModel = mock(TableColumnModel.class);
-            
+
             Vector<TableColumn> columnVector = new Vector<TableColumn>();
             for (int i = 0; i < 3; ++i) {
                 final String header = "column" + i;
                 final TableColumn col = mock(TableColumn.class, header);
 
                 checking(new Expectations() {{
-                    one(col).getHeaderValue(); will(returnValue(header));
+                    one(col).getHeaderValue();
+                    will(returnValue(header));
                 }});
-                
+
                 columnVector.add(col);
             }
-            
+
             final Enumeration<TableColumn> columns = columnVector.elements();
-            
+
             checking(new Expectations() {{
-                one(jTableOperator).getTableHeader(); will(returnValue(tableHeader));
-                one(tableHeader).getColumnModel(); will(returnValue(columnModel));
-                one(columnModel).getColumns(); will(returnValue(columns));
+                one(jTableOperator).getTableHeader();
+                will(returnValue(tableHeader));
+                one(tableHeader).getColumnModel();
+                will(returnValue(columnModel));
+                one(columnModel).getColumns();
+                will(returnValue(columns));
             }});
 
             specify(context.getTableHeaders(), containsInOrder("column0", "column1", "column2"));
         }
     }
-    
+
     public class OperatingOnTableWithColumnIndex extends OperatingOnTable {
         protected JTableOperator createMockJTableOperator() {
             column = "2";
             jTableOperator = mock(JTableOperator.class);
             checking(new Expectations() {{
-                one(jTableOperator).findCell(with(instanceOf(ColumnIndexTableCellChooser.class))); will(returnValue(coordinates));
+                one(jTableOperator).findCell(with(instanceOf(ColumnIndexTableCellChooser.class)));
+                will(returnValue(coordinates));
                 ignoring(jTableOperator).setComparator(with(any(EqualsStringComparator.class)));
             }});
-            
+
             return jTableOperator;
         }
     }
-    
+
     public class OperatingOnTableWithColumnHeaderName extends OperatingOnTable {
         protected JTableOperator createMockJTableOperator() {
             column = "someColumn";
             jTableOperator = mock(JTableOperator.class);
             checking(new Expectations() {{
-                one(jTableOperator).findCell(with(instanceOf(ColumnNameTableCellChooser.class))); will(returnValue(coordinates));
+                one(jTableOperator).findCell(with(instanceOf(ColumnNameTableCellChooser.class)));
+                will(returnValue(coordinates));
                 ignoring(jTableOperator).setComparator(with(any(EqualsStringComparator.class)));
             }});
-            
+
             return jTableOperator;
         }
     }
-    
+
     @SuppressWarnings("unchecked")
     private <T> Matcher<T> instanceOf(Class<T> type) {
         return (Matcher<T>) new IsInstanceOf(type);
     }
-    
+
     public abstract class OperatingOnTable {
         protected String column;
+
         protected abstract JTableOperator createMockJTableOperator();
-        
+
         public TableOperator create() {
             return new TableOperator(createMockJTableOperator());
         }
 
         public void checksCellIsSelected() {
             checking(new Expectations() {{
-                one(jTableOperator).isCellSelected(coordinates.y, coordinates.x); will(returnValue(true));
+                one(jTableOperator).isCellSelected(coordinates.y, coordinates.x);
+                will(returnValue(true));
             }});
-            
+
             specify(context.isCellSelected(row, column));
         }
-        
+
         public void selectsCell() {
             checking(new Expectations() {{
                 one(jTableOperator).selectCell(coordinates.y, coordinates.x);
             }});
-            
+
             context.selectCell(row, column);
         }
-        
+
         public void setsCellValue() {
             final Object newValue = new Object();
             checking(new Expectations() {{
                 one(jTableOperator).setValueAt(newValue, coordinates.y, coordinates.x);
             }});
-            
+
             context.setCellValue(newValue, row, column);
         }
-        
+
         public void typesIntoCell() {
             final Object newValue = new Object();
             checking(new Expectations() {{
                 one(jTableOperator).changeCellObject(coordinates.y, coordinates.x, newValue);
             }});
-            
+
             context.typeIntoCell(newValue, row, column);
         }
-        
+
         public void clearsCell() {
             checking(new Expectations() {{
                 one(jTableOperator).changeCellObject(coordinates.y, coordinates.x, "");
             }});
-            
+
             context.clearCell(row, column);
         }
-        
+
         public void callsPopupOnCell() {
             final JPopupMenu popupMenu = dummy(JPopupMenu.class);
             checking(new Expectations() {{
-                one(jTableOperator).callPopupOnCell(coordinates.y, coordinates.x); will(returnValue(popupMenu));
+                one(jTableOperator).callPopupOnCell(coordinates.y, coordinates.x);
+                will(returnValue(popupMenu));
             }});
-            
+
             specify(context.callPopupOnCell(row, column).getSource(), popupMenu);
         }
     }
-    
-    public class ExtractingProperties {
-        private Component renderComponent;
 
-        public TableOperator create() {
-            renderComponent = dummy(Component.class);
-            TableOperator tableOperator = new TableOperator(dummy(JTableOperator.class)) {
-                Component getCellRendererComponent(String row, String columnIdentifier) {
-                    return renderComponent;
-                }
-            };
-            
-            return tableOperator;
-        }
-        
-        public void extractsCellProperties() {
-            final PropertyExtractor propertyExtractor = injectMockToContext(PropertyExtractor.class);
-            final Map<String, Object> props = new HashMap<String, Object>();
-            checking(new Expectations() {{
-                one(propertyExtractor).extractProperties(renderComponent); will(returnValue(props));
-            }});
-            
-            specify(context.getCellProperties(row, "someColumn"), props);
-        }
-    }
 }
