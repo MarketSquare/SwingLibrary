a5c32f703f374ebb1159e77e03ca1a400c201361
janne.t.harkonen <devnull@localhost>
2011-08-24 08:10:48 +0000

    run_demo.py: add command line switch to start application

--- demo/run_demo.py
+++ demo/run_demo.py
@@ -5,10 +5,24 @@ import sys
 from subprocess import call
 from glob import glob
 
+TESTAPP = 'org.robotframework.swing.testapp.examplesut.TodoListApplication'
+
+def _get_jars():
+    dir = os.path.dirname(__file__)
+    return glob(os.path.join(dir, 'lib',
+                'swinglibrary-*-jar-with-dependencies.jar'))
+
+def _set_classpath():
+    os.environ['CLASSPATH'] = os.pathsep.join(_get_jars())
+
+def _parse_command():
+    if sys.argv[1] == 'startapp':
+        return ['java', TESTAPP]
+    return ['jybot', '--outputdir',  'results'] + sys.argv[1:]
 
 if __name__ == '__main__':
-    dir = os.path.dirname(__file__)
-    jars = glob(os.path.join(dir, 'lib', 'swinglibrary-*-jar-with-dependencies.jar'))
-    os.environ['CLASSPATH'] = os.pathsep.join(jars)
-    call(["jybot", "--outputdir",  "results"] + sys.argv[1:],
-            shell=os.name=='nt')
+    if len(sys.argv) == 1:
+        print 'Usage: run_demo.py [startapp|path_to_tests]'
+        sys.exit(1)
+    _set_classpath()
+    call(_parse_command(), shell=os.name=='nt')
